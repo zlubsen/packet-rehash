6cc507f9036aa4a58326dafaaa3eddb74edb0e7a
Zeeger <zeeger@lubsen.eu>
2022-11-09 20:31:53 +0100

    Start of Player client using Tauri, Vite, Svelte, TailwindCSS.

---
+++ play-tauri/src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import svelteLogo from './assets/svelte.svg'
+    import Counter from './lib/Counter.svelte'
+
+    function play() {
+        alert("play!");
+    }
+
+    function pause() {
+        alert("pause!");
+    }
+
+    function rewind() {
+        alert("rewind!");
+    }
+</script>
+
+<main class="w-full h-screen">
+    <h1>Packet Play</h1>
+    <div class="flex flex-row flex-no-wrap justify-around w-full">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 cursor-pointer" on:click={play}>
+            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z" clip-rule="evenodd" />
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 cursor-pointer" on:click={pause}>
+            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z" clip-rule="evenodd" />
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 cursor-pointer" on:click={rewind}>
+            <path d="M9.195 18.44c1.25.713 2.805-.19 2.805-1.629v-2.34l6.945 3.968c1.25.714 2.805-.188 2.805-1.628V8.688c0-1.44-1.555-2.342-2.805-1.628L12 11.03v-2.34c0-1.44-1.555-2.343-2.805-1.629l-7.108 4.062c-1.26.72-1.26 2.536 0 3.256l7.108 4.061z" />
+        </svg>
+    </div>
+
+</main>
+
+<style>
+</style>
---
+++ play-tauri/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{html,js,ts}",
+        "./index.html",
+        './src/**/*.svelte',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
