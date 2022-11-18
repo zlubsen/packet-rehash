09ff2d8b8145380c3710bbec27a709791fc83d64
Zeeger <zeeger@lubsen.eu>
2022-11-18 08:56:08 +0100

    Creating the UI

--- play-tauri/src/App.svelte
+++ play-tauri/src/App.svelte
@@ -3,7 +3,6 @@
     import { open } from '@tauri-apps/api/dialog';
     import { appWindow } from "@tauri-apps/api/window";
     import { asString } from 'date-format';
-    // const format = require('date-format');
 
     appWindow.listen("player_event_error", ({ event, payload }) => {
         console.log(event);
@@ -14,7 +13,7 @@
         console.log(payload);
     });
     appWindow.listen("player_event_state", ({ event, payload }) => {
-        player_state = payload;
+        player_state = payload.state;
         console.log(event);
         console.log(payload);
     });
@@ -34,14 +33,17 @@
         console.log(payload);
     });
 
-    let SETTINGS = {
+    let DEFAULT_SETTINGS = {
         destination: "192.168.8.255:3000",
         source_port: 3001,
         ttl: 1
     }
-    let player_state = {
-        state: 'Load a file'
-    };
+
+    let has_file_loaded = false;
+    let setting_destination = DEFAULT_SETTINGS.destination;
+    let setting_source_port = DEFAULT_SETTINGS.source_port;
+    let setting_ttl = DEFAULT_SETTINGS.ttl;
+    let player_state = 'Open a recording';
     let player_position = {
         position: 0,
         max_position: 0,
@@ -49,13 +51,13 @@
         time_total: 0
     }
 
-    function update_settings() {
-        invoke('cmd_update_settings', SETTINGS)
+    function update_settings(event: MouseEvent) {
+        invoke('cmd_update_settings', DEFAULT_SETTINGS)
             .then((message) => console.log(message))
             .catch((error) => console.error(error));
     }
 
-    async function open_file() {
+    async function open_file(event: MouseEvent) {
         const selected = await open({
             multiple: false,
             filters: [{
@@ -72,8 +74,11 @@
             invoke('cmd_open', {
                 filePath: selected
             })
-                .then((message) => console.log(message))
-                .catch((error) => console.error(error));
+                .then((message) => has_file_loaded = true)
+                .catch((error) => {
+                    has_file_loaded = false;
+                    console.error(error);
+                });
         }
     }
 
@@ -102,24 +107,30 @@
     <div class="flex justify-between p-0 border">
         <div class="flex-auto w-1/2 p-0">
             Player status
-            <div class="grid grid-cols-2 gap-4">
-                <div class="">State</div>
-                <div class="">{player_state.state}</div>
-                <div class="">Position</div>
-                <div class="">{player_position.position}/{player_position.max_position}</div>
-                <div class="">Time</div>
-                <div class="">{asString('hh:mm:ss', player_position.time_position)}/{asString('hh:mm:ss', player_position.time_total)}</div>
-            </div>
+            {#if has_file_loaded}
+                <div class="grid grid-cols-2 gap-4">
+                    <div class="">State</div>
+                    <div class="">{player_state.state}</div>
+                    <div class="">Position</div>
+                    <div class="">{player_position.position}/{player_position.max_position}</div>
+                    <div class="">Time</div>
+                    <div class="">{asString('hh:mm:ss', player_position.time_position)}/{asString('hh:mm:ss', player_position.time_total)}</div>
+                </div>
+            {:else}
+                <div>
+                    No file loaded
+                </div>
+            {/if}
         </div>
         <div class="flex-auto w-1/2 p-0">
             Settings
             <div class="grid grid-cols-2 gap-4">
                 <div class="">Destination</div>
-                <div class="">value</div>
+                <div class="">{setting_destination}</div>
                 <div class="">Source port</div>
-                <div class="">value</div>
+                <div class="">{setting_source_port}</div>
                 <div class="">TTL</div>
-                <div class="">value</div>
+                <div class="">{setting_ttl}</div>
             </div>
         </div>
     </div>
@@ -129,21 +140,29 @@
     </div>
 
     <div class="flex flex-row flex-no-wrap justify-evenly w-full py-2">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control hover:bg-blue-100" on:click={play}>
-            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z" clip-rule="evenodd" />
-        </svg>
+        <button on:click={play} class="player-control">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control-icon">
+                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z" clip-rule="evenodd" />
+            </svg>
+        </button>
 
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control hover:bg-blue-100" on:click={pause}>
-            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z" clip-rule="evenodd" />
-        </svg>
+        <button on:click={pause} class="player-control">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control-icon">
+                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z" clip-rule="evenodd" />
+            </svg>
+        </button>
 
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control hover:bg-blue-100" on:click={rewind}>
-            <path fill-rule="evenodd" d="M9.195 18.44c1.25.713 2.805-.19 2.805-1.629v-2.34l6.945 3.968c1.25.714 2.805-.188 2.805-1.628V8.688c0-1.44-1.555-2.342-2.805-1.628L12 11.03v-2.34c0-1.44-1.555-2.343-2.805-1.629l-7.108 4.062c-1.26.72-1.26 2.536 0 3.256l7.108 4.061z" clip-rule="evenodd" />
-        </svg>
+        <button on:click={rewind} class="player-control">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control-icon">
+                <path fill-rule="evenodd" d="M9.195 18.44c1.25.713 2.805-.19 2.805-1.629v-2.34l6.945 3.968c1.25.714 2.805-.188 2.805-1.628V8.688c0-1.44-1.555-2.342-2.805-1.628L12 11.03v-2.34c0-1.44-1.555-2.343-2.805-1.629l-7.108 4.062c-1.26.72-1.26 2.536 0 3.256l7.108 4.061z" clip-rule="evenodd" />
+            </svg>
+        </button>
 
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control hover:bg-blue-100" on:click={open_file}>
-            <path fill-rule="evenodd" d="M19.906 9c.382 0 .749.057 1.094.162V9a3 3 0 00-3-3h-3.879a.75.75 0 01-.53-.22L11.47 3.66A2.25 2.25 0 009.879 3H6a3 3 0 00-3 3v3.162A3.756 3.756 0 014.094 9h15.812zM4.094 10.5a2.25 2.25 0 00-2.227 2.568l.857 6A2.25 2.25 0 004.951 21H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-2.227-2.568H4.094z" />
-        </svg>
+        <button on:click={open_file} class="player-control">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control-icon">
+                <path fill-rule="evenodd" d="M19.906 9c.382 0 .749.057 1.094.162V9a3 3 0 00-3-3h-3.879a.75.75 0 01-.53-.22L11.47 3.66A2.25 2.25 0 009.879 3H6a3 3 0 00-3 3v3.162A3.756 3.756 0 014.094 9h15.812zM4.094 10.5a2.25 2.25 0 00-2.227 2.568l.857 6A2.25 2.25 0 004.951 21H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-2.227-2.568H4.094z" />
+            </svg>
+        </button>
 <!--        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 20 20" fill="currentColor" class="player-control w-16 h-16 hover:bg-blue-100" on:click={open_file}>-->
 <!--            <path fill-rule="evenodd" d="M7.27 1.047a1 1 0 0 1 1.46 0l6.345 6.77c.6.638.146 1.683-.73 1.683H1.656C.78 9.5.326 8.455.926 7.816L7.27 1.047zM.5 11.5a1 1 0 0 1 1-1h13a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1h-13a1 1 0 0 1-1-1v-1z" clip-rule="evenodd" />-->
 <!--        </svg>-->
@@ -157,6 +176,13 @@
     }
 
     .player-control {
-        @apply w-12 h-12 cursor-pointer;
+        @apply border border-black bg-white;
+    }
+    .player-control:hover {
+        @apply bg-blue-100 border-blue-200;
+    }
+
+    .player-control-icon {
+        @apply w-12 h-12;
     }
 </style>
