e805eb64fa2a259a0dbd9831326578e8f13cd815
Zeeger <zeeger@lubsen.eu>
2022-11-20 21:32:25 +0100

    Working the UI
    - styling
    - toggle between themes

--- play-tauri/src/App.svelte
+++ play-tauri/src/App.svelte
@@ -2,7 +2,13 @@
     import { invoke } from '@tauri-apps/api/tauri'
     import { open } from '@tauri-apps/api/dialog';
     import { appWindow } from "@tauri-apps/api/window";
-    import { asString } from 'date-format';
+    import { onMount } from 'svelte';
+    import { themeChange } from 'theme-change';
+
+    onMount(() => {
+        themeChange(false)
+        // 👆 false parameter is required for svelte
+    })
 
     interface Settings {
         destination: string,
@@ -173,7 +179,13 @@
 </script>
 
 <main class="main-viewport flex flex-col justify-between bg-base-100 border">
-    <h1 class="py-2 grow-0">Packet Play</h1>
+    <h1 class="py-2 grow-0 text-primary">Packet Play</h1>
+
+    <label class="swap swap-rotate absolute top-0 right-0 text-primary-focus">
+        <input type="checkbox" data-toggle-theme="bumblebee,coffee" data-act-class="ACTIVECLASS"/>
+        <svg class="swap-on fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
+        <svg class="swap-off fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
+    </label>
 
     <div class="grow flex justify-between p-0 bg-base-200">
         <div class="flex-auto w-1/2 p-0">
@@ -209,44 +221,52 @@
     </div>
 
     <div class="py-2 grow-0 px-4">
-        <progress class="progress progress-primary" value="{player_position.position}" max="{player_position.max_position}"></progress>
-        [{formatSecs(player_position.time_position_secs)}] / [{formatSecs(player_position.time_total_secs)}]
+        <progress class="progress progress-secondary" value="{player_position.position}" max="{player_position.max_position}"></progress>
+        <span class="text-secondary">[{formatSecs(player_position.time_position_secs)}] / [{formatSecs(player_position.time_total_secs)}]</span>
     </div>
 
-    <div class="grow-0 flex flex-row flex-no-wrap justify-evenly w-full py-2">
-        <button on:click={play} class="btn btn-primary btn-lg" disabled="{disableBtn(!canPlay(player_state))}" class:btn-active={isPlaying()}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control-icon">
-                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z" clip-rule="evenodd" />
-            </svg>
-        </button>
+    <div class="grow-0 flex flex-row flex-no-wrap justify-evenly w-full py-5">
+        <div class="tooltip tooltip-primary" data-tip="Replay recording">
+            <button on:click={play} class="btn btn-primary btn-lg" disabled="{disableBtn(!canPlay(player_state))}" class:btn-active={isPlaying()}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control-icon">
+                    <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </div>
 
-        <button on:click={pause} class="btn btn-primary btn-lg" disabled="{disableBtn(!canPause(player_state))}">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control-icon">
-                <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z" clip-rule="evenodd" />
-            </svg>
-        </button>
+        <div class="tooltip tooltip-primary" data-tip="Pause playback">
+            <button on:click={pause} class="btn btn-primary btn-lg" disabled="{disableBtn(!canPause(player_state))}">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control-icon">
+                    <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </div>
 
-        <button on:click={rewind} class="btn btn-primary btn-lg" disabled="{disableBtn(!canRewind(player_state))}">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control-icon">
-                <path fill-rule="evenodd" d="M9.195 18.44c1.25.713 2.805-.19 2.805-1.629v-2.34l6.945 3.968c1.25.714 2.805-.188 2.805-1.628V8.688c0-1.44-1.555-2.342-2.805-1.628L12 11.03v-2.34c0-1.44-1.555-2.343-2.805-1.629l-7.108 4.062c-1.26.72-1.26 2.536 0 3.256l7.108 4.061z" clip-rule="evenodd" />
-            </svg>
-        </button>
+        <div class="tooltip tooltip-primary" data-tip="Rewind to beginning">
+            <button on:click={rewind} class="btn btn-primary btn-lg" disabled="{disableBtn(!canRewind(player_state))}">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="player-control-icon">
+                    <path fill-rule="evenodd" d="M9.195 18.44c1.25.713 2.805-.19 2.805-1.629v-2.34l6.945 3.968c1.25.714 2.805-.188 2.805-1.628V8.688c0-1.44-1.555-2.342-2.805-1.628L12 11.03v-2.34c0-1.44-1.555-2.343-2.805-1.629l-7.108 4.062c-1.26.72-1.26 2.536 0 3.256l7.108 4.061z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </div>
 
-        <button on:click={open_file} class="btn btn-primary btn-lg">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 20 20" fill="currentColor" class="player-control-icon">
-                <path fill-rule="evenodd" d="M7.27 1.047a1 1 0 0 1 1.46 0l6.345 6.77c.6.638.146 1.683-.73 1.683H1.656C.78 9.5.326 8.455.926 7.816L7.27 1.047zM.5 11.5a1 1 0 0 1 1-1h13a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1h-13a1 1 0 0 1-1-1v-1z" clip-rule="evenodd" />
-            </svg>
-        </button>
+        <div class="tooltip tooltip-primary" data-tip="Open a recording">
+            <button on:click={open_file} class="btn btn-primary btn-lg">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -2 20 20" fill="currentColor" class="player-control-icon">
+                    <path fill-rule="evenodd" d="M7.27 1.047a1 1 0 0 1 1.46 0l6.345 6.77c.6.638.146 1.683-.73 1.683H1.656C.78 9.5.326 8.455.926 7.816L7.27 1.047zM.5 11.5a1 1 0 0 1 1-1h13a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1h-13a1 1 0 0 1-1-1v-1z" clip-rule="evenodd" />
+                </svg>
+            </button>
+        </div>
     </div>
 </main>
 
 <style>
     .main-viewport {
         @apply h-screen w-screen overflow-x-hidden overflow-y-hidden;
-        @apply p-0 m-0;
+        /*@apply p-0 m-0;*/
         min-height: -webkit-fill-available;
-        width: 90vw;
-        height: 90vh;
+        /*width: 90vw;*/
+        /*height: 90vh;*/
     }
 
     .player-control-icon {
